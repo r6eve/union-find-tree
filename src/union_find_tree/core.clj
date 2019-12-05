aa74dac25d652e44422c268e337c6f9f29eecdec
r6eve <r6eve179f@gmail.com>
2019-12-05 19:28:58 +0900

    Add docstrings

--- src/union_find_tree/core.clj
+++ src/union_find_tree/core.clj
@@ -1,13 +1,16 @@
 (ns union-find-tree.core
+  "Library designed to manipulate union-find tree."
   (:refer-clojure :exclude [find]))
 
 (defrecord UnionFind [^long parent ^long rank])
 
 (defn make
+  "Initializes tree with `n` elements (0, 1, ..., `n` - 1)."
   [n]
   (into-array (map #(->UnionFind % 0) (range n))))
 
 (defn find
+  "Returns a root of a tree of the given `x`."
   [^"[Lunion_find_tree.core.UnionFind;" s x]
   (letfn [(doit [x]
             (let [^UnionFind sx (aget s x)]
@@ -19,6 +22,7 @@
     (doit x)))
 
 (defn union!
+  "Combines the trees that `x` and `y` belong to."
   [^"[Lunion_find_tree.core.UnionFind;" s x y]
   (let [px (find s x)
         py (find s y)
@@ -32,5 +36,6 @@
           (aset s px (->UnionFind (.parent spx) (inc (.rank spx)))))))))
 
 (defn same-set?
+  "Returns true if `x` and `y` are belong to the same tree."
   [s x y]
   (= (find s x) (find s y)))
